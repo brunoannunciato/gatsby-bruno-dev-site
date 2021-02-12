0377674e1591b8bb4f2e055322caf9a6de47db8e
brunoannunciato <brunobannunciato@gmail.com>
2021-02-12 17:58:48 -0300

    :wrench: Início página inicial blog

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import './styles/blog.scss'
+
+const BlogPage = () => (
+  <Layout>
+    <SEO title="Blog" />
+      BLOG
+  </Layout>
+)
+
+export default BlogPage
