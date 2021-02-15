e92348835458f23f01505297b9005aaaff17d69e
brunoannunciato <brunobannunciato@gmail.com>
2021-02-15 16:06:32 -0300

    :sparkles: Estilização página de portifolio

--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -1,14 +1,55 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import './styles/portifolio.scss'
 
-const PortifolioPage = () => (
-  <Layout>
-    <SEO title="Portifólio" />
-    <section className="portifolio">
-    </section>
-  </Layout>
-)
+import JobItem from '../components/JobItem'
+
+const PortifolioPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+  {
+    allMarkdownRemark(filter: {frontmatter: {section: {eq: "portifolio"}}}) {
+      edges {
+        node {
+          frontmatter {
+            category
+            description
+            title
+            technologies
+          }
+          html
+        }
+      }
+    }
+  }
+  `)
+
+  const jobs = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Portifólio" />
+      <section className="portifolio">
+        {
+          jobs.map((job, index) => {
+            const { title, description, category, technologies } = job.node.frontmatter
+
+            return (
+              <JobItem
+                key={ index }
+                title={ title }
+                description={ description }
+                category={ category }
+                technologies={ technologies }
+                thumbnail={ job.node.html }
+              />
+            )
+          })
+        }
+      </section>
+    </Layout>
+  )
+}
 
 export default PortifolioPage
