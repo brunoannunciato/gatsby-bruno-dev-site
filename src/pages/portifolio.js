e68aa36bb601615261bd8d923d0a1d6665e3fd06
brunoannunciato <brunobannunciato@gmail.com>
2021-02-15 13:59:31 -0300

    :wrench: Estrutura para página de portifólio

---
+++ src/pages/portifolio.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import './styles/portifolio.scss'
+
+const PortifolioPage = () => (
+  <Layout>
+    <SEO title="Portifólio" />
+    <section className="portifolio">
+    </section>
+  </Layout>
+)
+
+export default PortifolioPage
