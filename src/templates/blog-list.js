626a9ed3eaba4fff6633773b1b70206e61eb1cb8
Bruno <brunobannunciato@gmail.com>
2021-02-21 14:18:19 -0300

    :recycle: Separação de renderização de posts direto na página do blog

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import PostItem from '../components/PostItem'
 import Pagination from '../components/Pagination'
+import Search from '../components/Search'
+import BlogPosts from './blog-posts'
+import './styles/blog-list.scss'
 
 const BlogList = (props) => {
   const postList = props.data.allMarkdownRemark.edges
@@ -13,43 +15,39 @@ const BlogList = (props) => {
   const isLast = currentPage === numPages
   const prevPage = currentPage === 2 ? '/blog' : `/blog/${currentPage - 1}`
   const nextPage = `/blog/${currentPage + 1 }`
+  const [ isSearching, setIsSearching ] = useState(false)
+
+  const handleIsSearching = inputValue => {
+    if (inputValue.length === 0) {
+      setIsSearching(false)
+      return
+    }
+
+    setIsSearching(true)
+  }
 
   return (
     <Layout>
       <SEO title="Blog" />
+        <div className="search-box">
+          <Search handleIsSearching={ handleIsSearching } isSearching={ isSearching }/>
+        </div>
+
         {
-          postList.map(({node: {
-            frontmatter: {
-              category,
-              date,
-              description,
-              title
-            },
-            fields: { slug },
-            timeToRead
-          }}) => {
-            return (
-              <PostItem
-                key={ slug }
-                slug={ slug }
-                title={ title }
-                category={ category }
-                description={ description }
-                date={ date }
-                timeToRead={ timeToRead }
-              />
-            )
-          })
+          !isSearching &&
+          <>
+            <BlogPosts postList={ postList } />
+            <Pagination
+              isFirst = { isFirst }
+              isLast = { isLast } 
+              prevPage = { prevPage }
+              nextPage = { nextPage }
+              currentPage = { currentPage }
+              numPages = { numPages }
+            />
+          </>
         }
 
-        <Pagination
-          isFirst = { isFirst }
-          isLast = { isLast } 
-          prevPage = { prevPage }
-          nextPage = { nextPage }
-          currentPage = { currentPage }
-          numPages = { numPages }
-        />
     </Layout>
   )
 }
